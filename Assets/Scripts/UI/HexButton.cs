c28bb8709ae2c20fdde41e42b98d2a9dc38200f5
jtrotter <jtrotter@onetest.com.au>
2017-06-17 11:58:59 +1000

    UI improvements to main menu and challenges

---
+++ Assets/Scripts/UI/HexButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+[RequireComponent(typeof(RectTransform))]
+public class HexButton : MonoBehaviour {
+
+	[SerializeField]
+	private Sprite upSprite;
+
+	[SerializeField]
+	private Sprite downSprite;
+
+	[SerializeField]
+	private float heightShrinkAmount = 5f;
+
+	private Image image;
+	private RectTransform rectTransform;
+
+	private Vector2 upSize;
+	private Vector3 upPos;
+	private Vector2 downSize;
+	private Vector3 downPos;
+
+	void Awake() {
+		image = GetComponent<Image> ();
+		rectTransform = GetComponent<RectTransform> ();
+
+		upSize = rectTransform.sizeDelta;
+		upPos = rectTransform.localPosition;
+		downSize = new Vector2 (upSize.x, upSize.y - heightShrinkAmount);
+		downPos = upPos - new Vector3 (0, heightShrinkAmount / 2f, 0);
+	}
+
+	public void onDown() {
+		rectTransform.sizeDelta = downSize;
+		rectTransform.localPosition = downPos;
+		image.sprite = downSprite;
+	}
+
+	public void onUp() {
+		rectTransform.sizeDelta = upSize;
+		rectTransform.localPosition = upPos;
+		image.sprite = upSprite;
+	}
+
+}
